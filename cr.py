41d8de9cc5aa746f267dc8bc9afc2564a92e7e60
Nadine Kraft <33866910+NadineKraft@users.noreply.github.com>
2020-09-02 09:44:04 +0200

    Update cr.py

--- cr.py
+++ cr.py
@@ -6,9 +6,12 @@ from crispr.Crispr import Crispr
 from misc.Color import RED, BLUE, YELLOW, CYAN
 from roi_source.BedFileRoiSource import BedFileRoiSource
 from roi_source.JsonFileRoiSource import JsonFileRoiSource
+from roi_source.JsonStringRoiSource import JsonStringRoiSource
 from target_sink.BedFileTargetSink import BedFileTargetSink, Colors
 from target_sink.JsonFileTargetSink import JsonFileTargetSink
-from fasta.FastaDictionary import SimpleFastaDictionary
+# from target_sink.JsonStringTargetSink import JsonStringTargetSink
+from fastadict.SimpleFastaDictionary import SimpleFastaDictionary
+# from fastadict import FastaDictionary
 from target_index.FastaFileTargetIndex import FastaFileTargetIndex
 from scorer.GcContentScorer import GcContentScorer
 from scorer.DoenchScorer import DoenchScorer
@@ -17,10 +20,12 @@ from scorer.ConstantScorer import ConstantScorer
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find CRISPR-targets")
-    parser.add_argument("--fasta", type=str, help="Input fasta filename", required=True)
-    parser.add_argument("--input_roi", type=str, help="Allowed roi input file types: .bed, .json", required=True)
+    parser.add_argument("--fastadict", type=str, help="Input fastadict filename", required=True)
+    parser.add_argument("--input_roi", type=str, help="Allowed roi input file types: .bed, .json, _json", required=True)
     parser.add_argument("--scorer", type=str, choices=['GC', 'Doench', 'MIT'],
                         help="Allowed scoring methods: GC, Doench, MIT")
+    parser.add_argument("--input roi_type", type=str, choices=['bed', 'json', 'auto'],
+                        help="Allowed input types: bed, json, auto", default='auto')
     parser.add_argument("--output", type=str, help="output filename", required=True)
     parser.add_argument("--frame", default='200', type=int, help="frame size")
     parser.add_argument("--score_minimum", default='0.0', type=float,
@@ -34,6 +39,8 @@ if __name__ == "__main__":
     frame = args.frame
     scorer_name = args.scorer
     score_minimum = args.score_minimum
+
+    # TODO: bei choice 'auto' raten aufgrund der datei endung, ansonsten wie scorer
 
     if input_roi.endswith('.bed'):
         roi_source = BedFileRoiSource(
@@ -55,8 +62,19 @@ if __name__ == "__main__":
         target_sink = JsonFileTargetSink(
             output
         )
+    elif input_roi.endswith('_json'):
+        roi_source = JsonStringRoiSource(
+            input_roi,
+            frame
+        )
+        # target_sink = JsonStringTargetSink()
+        # TODO: JsonStringTargetSink doesn't work yet. Can't access the json dump in app.py
+        target_sink = JsonFileTargetSink(
+            'GiveMeTargets'
+        )
+
     else:
-        print("Wrong roi input file format. Please enter a *.bed or *.txt file")
+        print("Wrong roi input file format. Please enter a *.bed or *.json file")
 
     target_index = FastaFileTargetIndex(
         fasta,
@@ -72,7 +90,5 @@ if __name__ == "__main__":
     else:
         print("Using default Constant Scorer")
         scorer = ConstantScorer()
-
-
 
     Crispr(roi_source, target_index, scorer, target_sink, score_minimum).perform()
