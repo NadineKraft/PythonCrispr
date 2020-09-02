ed15de958d55a06571c6a04333899ae38f52b096
Nadine Kraft <33866910+NadineKraft@users.noreply.github.com>
2020-09-02 09:32:21 +0200

    Update Crispr.py

--- crispr/Crispr.py
+++ crispr/Crispr.py
@@ -6,11 +6,15 @@ from target_sink.TargetSink import TargetSink
 
 class Crispr:
 
-    def __init__(self, roi_source: RoiSource, target_index: TargetIndex, scorer: Scorer, target_sink: TargetSink,
-                 score_minimum: float):
+    def __init__(self, roi_source: RoiSource, target_index: TargetIndex, mit_scorer: Scorer, gc_scorer: Scorer,
+                 doench_scorer: Scorer,
+                 target_sink: TargetSink,
+                 score_minimum: int):
         self.roi_source = roi_source
         self.target_index = target_index
-        self.scorer = scorer
+        self.mit_scorer = mit_scorer
+        self.gc_scorer = gc_scorer
+        self.doench_scorer = doench_scorer
         self.target_sink = target_sink
         self.score_minimum = score_minimum
 
@@ -20,7 +24,9 @@ class Crispr:
                 print("processing: " + str(roi))
                 if self.target_index.is_acceptable(roi):
                     for target in self.target_index.targets(roi):
-                        if self.scorer.score(target) >= self.score_minimum:
-                            self.target_sink.append(roi, target, self.scorer.score(target))
+                        if self.mit_scorer.score(target) >= self.score_minimum:
+                            self.target_sink.append(roi, target, self.mit_scorer.score(target),
+                                                    self.gc_scorer.score(target), self.doench_scorer.score(target))
+
                 else:
                     print("ignoring: " + str(roi))
